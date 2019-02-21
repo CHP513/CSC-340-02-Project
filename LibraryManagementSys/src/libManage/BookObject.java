3759f0818297b75b879b65b922c79eeb9828accb
CHP513 <42984594+CHP513@users.noreply.github.com>
2019-02-20 20:36:23 -0500

    Add files via upload

---
+++ LibraryManagementSys/src/libManage/BookObject.java
@@ -0,0 +1,22 @@
+
+package libManage;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+
+public class BookObject {
+    
+    String author;
+    String title;
+    String imageLink;
+
+    public BookObject(String Author, String Title, String ImageLink) {
+        author = Author;
+        title = Title;
+        imageLink = ImageLink;
+    }
+
+    
+}
